f267409335df6e7e0e9ae93fa15bee6d367fffe2
Jaime Pillora <dev@jpillora.com>
2019-04-01 20:35:59 +1100

    improve test diff, pre-complete

--- example/application/main.go
+++ example/application/main.go
@@ -9,17 +9,12 @@ import (
 var version = "0.0.0"
 
 func main() {
-	//configuration with defaults
-	app := foo.App{
-		Ping: "hello",
-		Pong: "world",
-	}
-	//parse config, note the library version, and extract the
-	//repository link from the config package import path
-	opts.New(&app).
-		Name("foo").
-		Version(version).
-		PkgRepo(). //includes the infered URL to the package in the help text
-		Parse().
-		Run()
+	//new app with some defaults
+	app := foo.App{Ping: "hello", Pong: "world"}
+	opts.
+		New(&app).        //infer CLI name from package name "foo"
+		Version(version). //use version string set at compile time
+		PkgRepo().        //infer the repo URL and include in the help text
+		Parse().          //exits with 1 if any problems
+		RunFatal()        //executes App's Run method
 }
