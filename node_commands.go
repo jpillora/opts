f29fcd8cf51a3df300b0fe5d4f9f567277379b6e
Gary Miller <miller.garym@gmail.com>
2022-08-08 17:50:18 +1000

    new struct and end

--- node_commands.go
+++ node_commands.go
@@ -4,7 +4,64 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"reflect"
+	"strings"
 )
+
+type subCmdHolderI interface {
+	isSubCmdHolder()
+}
+
+type subCmdHolder struct{}
+
+func (subCmdHolder) isSubCmdHolder() {}
+
+//NewStruct adds multiple Opts instances under the sub-command name.
+//The name can be a slash separated path.
+func NewStruct(path string, subs ...Opts) Opts {
+	names := strings.Split(path, "/")
+	fn := func(name string) *node {
+		cmd := newNode(reflect.ValueOf(&subCmdHolder{}))
+		cmd.name = names[0]
+		return cmd
+	}
+	return addStruct(fn, path, subs...)
+}
+
+func (n *node) AddStruct(path string, subs ...Opts) Opts {
+	names := strings.Split(path, "/")
+	fn := func(name string) *node {
+		return n.newStruct(names[0])
+	}
+	return addStruct(fn, path, subs...)
+}
+
+func addStruct(fn func(string) *node, name string, subs ...Opts) Opts {
+	names := strings.Split(name, "/")
+	cmd := fn(names[0])
+	cmd0 := cmd
+	for _, name0 := range names[1:] {
+		cmd1 := cmd0.newStruct(name0)
+		cmd0.AddCommand(cmd1)
+		cmd0 = cmd1
+	}
+	for _, sub := range subs {
+		cmd0.AddCommand(sub)
+	}
+	return cmd
+}
+
+func (n *node) newStruct(name string) *node {
+	if sub, exists := n.cmds[name]; exists {
+		return sub
+	} else {
+		sub := newNode(reflect.ValueOf(&subCmdHolder{}))
+		sub.name = name
+		sub.parent = n
+		n.cmds[name] = sub
+		return sub
+	}
+}
 
 func (n *node) AddCommand(cmd Opts) Opts {
 	sub, ok := cmd.(*node)
@@ -28,9 +85,19 @@ func (n *node) AddCommand(cmd Opts) Opts {
 		n.errorf("cannot add command, please set a Name()")
 		return n
 	}
-	if _, exists := n.cmds[sub.name]; exists {
-		n.errorf("cannot add command, '%s' already exists", sub.name)
-		return n
+	if sub0, exists := n.cmds[sub.name]; exists {
+		if _, ok1 := sub0.item.val.Interface().(subCmdHolderI); ok1 {
+			if _, ok2 := sub.item.val.Interface().(subCmdHolderI); !ok2 {
+				// replace sub cmd holding with provided sub cmd
+				for k, v := range sub0.cmds {
+					sub.cmds[k] = v
+					v.parent = sub
+				}
+			}
+		} else {
+			n.errorf("cannot add command, '%s' already exists", sub.name)
+			return n
+		}
 	}
 	sub.parent = n
 	n.cmds[sub.name] = sub
